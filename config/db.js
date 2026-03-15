43de208ac2689994cc10a216164a9338d7835986
EXBSON JAIR MENDEZ PARRA <exbsonmendez@gmail.com>
2026-03-15 00:17:28 -0500

    Summary (required)
    Actualización con phpmyadmin

--- config/db.js
+++ config/db.js
@@ -1,19 +1,20 @@
 const mysql = require("mysql")
 
 const db = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "",
-database: "tutorias",
-port: 3306
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "tutorias",
+  port: 3306,
+  multipleStatements: true
 })
 
-db.connect((err)=>{
-if(err){
-console.log("❌ Error conectando a MySQL:", err)
-}else{
-console.log("✅ Conectado a MySQL")
-}
+db.connect((err) => {
+  if (err) {
+    console.error("❌ Error conectando a MySQL:", err)
+  } else {
+    console.log("✅ Conectado a MySQL")
+  }
 })
 
 module.exports = db
